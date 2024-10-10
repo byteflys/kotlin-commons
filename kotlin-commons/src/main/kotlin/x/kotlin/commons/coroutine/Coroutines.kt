20775e29e21128e65c29423bfb77c3822e8999ed
easing <easing@onyx-international.com>
2024-10-10 10:50:11 +0800

    kotlin commons

--- kotlin-commons/src/main/kotlin/x/kotlin/commons/coroutine/Coroutines.kt
+++ kotlin-commons/src/main/kotlin/x/kotlin/commons/coroutine/Coroutines.kt
@@ -7,13 +7,13 @@ import kotlin.coroutines.startCoroutine
 object Coroutines {
 
     fun <T> createCoroutine(block: suspend () -> T): Continuation<Unit> {
-        val completion = x.kotlin.commons.coroutine.EmptyContinuation<T>()
+        val completion = EmptyContinuation<T>()
         val continuation = block.createCoroutine(completion)
         return continuation
     }
 
     fun <T> startCoroutine(block: suspend () -> T) {
-        val completion = x.kotlin.commons.coroutine.EmptyContinuation<T>()
+        val completion = EmptyContinuation<T>()
         block.startCoroutine(completion)
     }
 }
